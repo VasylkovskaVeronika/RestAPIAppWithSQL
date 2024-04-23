4fccbac3a188d708da3c8cad7eb8da355ec76aec
Veronika <veronika.novosad4@gmail.com>
2024-04-24 00:53:24 +0200

    WarehouseController.cs updated

--- WebApplication2/WebApplication2/Controllers/WarehouseController.cs
+++ WebApplication2/WebApplication2/Controllers/WarehouseController.cs
@@ -1,6 +1,39 @@
-﻿namespace WebApplication2.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Repositories;
 
-public class WarehouseController
+namespace WebApplication2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WarehouseController : ControllerBase
 {
-    
+    private readonly IConfiguration _configuration;
+    private readonly IWarehouseRepository _repo;
+
+    public WarehouseController(IConfiguration c, IWarehouseRepository r)
+    {
+        _configuration = c;
+        _repo = r;
+    }
+    [HttpPost]
+    public IActionResult CreateProductWarehouse(ProductWarehouse warehouse)
+    {
+        if (_repo.ProductWithIDExists(warehouse.IdProduct)
+            && _repo.WarehouseWithIDExists(warehouse.IdWarehouse)
+            && warehouse.Amount > 0)
+        {
+            if (_repo.AddProductRequirementsCompleted(warehouse.IdProduct, warehouse.Amount, warehouse.CreatedAt))
+            {
+                if (_repo.IsOrderCompleted(warehouse.IdProduct, warehouse.Amount))
+                {
+                    _repo.UpdateFullfilledAt(warehouse.IdProduct, warehouse.Amount);
+                    return Created("","Record inserted with ID = " + _repo.InsertProductWarehouse(warehouse.IdProduct, warehouse.IdWarehouse, warehouse.Amount));
+                }
+                return BadRequest("The order has already been completed.");
+            }
+            return NotFound("There is no suitable order.");
+        }
+        return NotFound("The product/warehouse with the given id does not exist");
+    }
 }
